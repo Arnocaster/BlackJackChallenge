1c5858d1a060642d40f644eaf9244c429f818230
Arnaud Peybernes <arnaud.peybernes@gmail.com>
2021-10-27 23:38:01 +0200

    ajout de turnDealer + refactor

--- js/gameEngine.js
+++ js/gameEngine.js
@@ -1,16 +1,1 @@
-/**
- * OBJET JEUX DE CARTES
- */
-const cardGame = {
-    globalSettings :{
-        cardNumber : 52,
-        joker : false,
-        cardGamesNumber : 6,
-    },
-    //CREE UN JEUX & MELANGE LES CARTES 
-    start : function(){
-        console.log('test');
-
-    }
-};
 
--- js/script.js
+++ js/script.js
@@ -6,17 +6,22 @@ const cardGame = {
         cardPerGameQuantity : 52,
         joker : false,
         cardGamesQuantity : 6,
-        lastSplit : 52,
+        minLastSplit : 52,
     },
     thisParty : {
         cardSet : [],
         playerToPlay : true,
+        isGameOver : false,
         player :{
+            name : 'Player',
             cards : [],
+            actualScore : 0,
             money : 500,
         },
-        dealer : {         
+        dealer : {
+            name : 'Dealer',         
             cards : [],
+            actualScore : 0,
             money : 50000,
         },
     },
@@ -33,17 +38,26 @@ const cardGame = {
         players.forEach(function(element){
             cardGame.pickCard(1,element.cards,cardSet);
         });  
-        //Il tire une seconde carte visible pour chaque joueur et fini par une cachée pour lui
+
+        // //Il tire une seconde carte visible pour chaque joueur et fini par une cachée pour lui
         players.forEach(function(element){
             cardGame.pickCard(1,element.cards,cardSet);
-        });  
-        //Affichage des cartes tirées
+        });
+        cardGame.calculateScore();  
+        cardGame.isGameOver();
+
+        // //Affichage des cartes tirées
         players.forEach(function(element){
-            cardGame.displayDeck(element.cards);
-        }); 
+            cardGame.displayHandOfCards(element);
+        });
         //A deux le croupier demande alors quelle action le joueur veut faire
-        //***TOUR DU JOUEUR
-        cardGame.playerTurn(thisParty);
+        // //***TOUR DU JOUEUR
+        cardGame.playerTurn();
+        cardGame.dealerTurn();
+        cardGame.playerTurn();
+        cardGame.dealerTurn();
+ 
+
         //Si le joueur a un blackjack alors il ne fait rien, le croupier joue
         //Si le joueur n'as pas de blackjack
         //-->Il peut demander une ou plusieurs cartes supplémnetaires
@@ -52,7 +66,7 @@ const cardGame = {
         //-->Splitter SSI il a deux cartes de même valeur : chaque carte devient un jeux à part entière pour lequel il peut agir indépendement.
         //Mais il ne peut plus faire de blackjack => Si le croupier a un blackjack  le joueur perd
         //-->Assurance : si la première carte du croupier est un as le joueur peut s'assurer : Il paye la moitié de sa mise Soit le croupier fait blackjack => 
-        //***TOUR DU DEALER
+        // //***TOUR DU DEALER
         //Si le dealer < 17 alors il tire
         //Si le dealer a As + 7 (=18) il s'arrete
         //Sinon il s'arrete
@@ -76,20 +90,20 @@ const cardGame = {
             // BOUCLE JEUX DE CARTE
             let thisCardGame = [];
             for (let cardColor = 0 ; cardColor < 4 ; cardColor++){
-            // Trèfle = Clubs ; Carreaux = Diamonds ; Coeur = Hearts ; Pique = Spades 
+                // Trèfle = Clubs ; Carreaux = Diamonds ; Coeur = Hearts ; Pique = Spades 
                 let thisColor = null;
                 switch(cardColor) {
                     case 0 : 
-                        thisColor = 'C';
+                        thisColor = '\u2663';
                         break;
                     case 1 : 
-                        thisColor = 'D';
+                        thisColor = '\u2666';
                         break;
                     case 2 : 
-                        thisColor = 'H';
+                        thisColor = '\u2665';
                         break;
                     case 3 : 
-                        thisColor = 'S';
+                        thisColor = '\u2660';
                         break;
 
                 }
@@ -124,59 +138,46 @@ const cardGame = {
             
         }
         //On coupe deux foix l'ensemble des jeux
-        cardGamesCollection = cardGame.sliceCards(cardGamesCollection);
-        cardGamesCollection = cardGame.sliceCards(cardGamesCollection);
+        // cardGamesCollection = cardGame.sliceCards(cardGamesCollection);
+        // cardGamesCollection = cardGame.sliceCards(cardGamesCollection);
         
         //On coupe le jeux a une position > 52 cartes
-        cardGamesCollection = cardGame.sliceCards(cardGamesCollection,cardGame.globalSettings.lastSplit);
+        cardGamesCollection = cardGame.sliceCards(cardGamesCollection,cardGame.globalSettings.minLastSplit);
         cardGame.thisParty.cardSet = cardGamesCollection;
     },
     //FONCTION : MELANGE UN JEUX
-    shuffleCards : function(cardGame){
+    shuffleCards : function(cardGameElt){
         let shuffledCardGame =[];
-        while (cardGame.length > 0){         
-            let cardPicked = cardGame.splice(Math.round(Math.random()*cardGame.length-1),1);
-            shuffledCardGame.push(cardPicked[0]);
-           
+        while (cardGameElt.length > 0){         
+            let cardPicked = cardGameElt.splice(Math.round(Math.random()*cardGameElt.length-1),1);
+            shuffledCardGame.push(cardPicked[0]); 
         }
         return shuffledCardGame;
     },
     //FONCTION : COUPE UN JEUX 
-    sliceCards : function(cardGame,minCardtoSlice){ 
+    sliceCards : function(cardGameElt,minCardtoSlice){ 
         if (minCardtoSlice === undefined){ minCardtoSlice = 0;}
-        let placeWhereISlice = Math.round((Math.random()*(cardGame.length-minCardtoSlice))+minCardtoSlice);
-        let after = cardGame.slice(placeWhereISlice);
-        let before = cardGame.slice (0,placeWhereISlice);
+        let placeWhereISlice = Math.round((Math.random()*(cardGameElt.length-minCardtoSlice))+minCardtoSlice);
+        let after = cardGameElt.slice(placeWhereISlice);
+        let before = cardGameElt.slice (0,placeWhereISlice);
         if (minCardtoSlice === 0){
             after.concat(before);
-            return cardGame;
+            return cardGameElt;
         } else {
             return before;
         }
         
     },
     //FONCTION : PIOCHE UNE OU PLUSIEURS CARTE DANS UN JEU
-    pickCard : function(numberOfCard,personToGive,cardGame){
-        let thisCard = cardGame.splice(0,1);
+    pickCard : function(numberOfCard,personToGive,cardGameElt){
+        let thisCard = cardGameElt.splice(0,1);
         //console.log('pickcard', personToGive);
-        personToGive.push(thisCard[0]);       
+        personToGive.push(thisCard[0]);     
     },
-    //FONCTION : MECANIQUE DE JEUX DU JOUEUR Selector = propriété thisparty
-    playerTurn : function(selector){
-        let playerAction = Number(prompt('Que voulez vous faire?\n 1)Tirer une carte 2)S\'arréter 3)Doubler 4)'));
-        switch (playerAction){
-            case 1 : 
-                cardGame.pickCard(1,selector.player.cards,selector.cardSet);
-                cardGame.displayDeck(selector.player.cards);
-                break;
-            default : 'défaut';
-        }
-    },
-    displayDeck : function(handOfCards){
-        let cardsToShow = [];
-        let handScore = 0;
-
-        handOfCards.forEach(function(element){
+    //FONCTION : Affichage d'une main
+    displayHandOfCards : function(handOfCards){
+        let cardsToShow = `${handOfCards.name}:`;
+        handOfCards.cards.forEach(function(element){
             let thisValue = null;
             switch (element.value){
                 case 11 : 
@@ -191,22 +192,84 @@ const cardGame = {
                 default :
                     thisValue = element.value;
                     break;
-              
+            
             }
-            //Si la carte est un as et que le score avec cette carte est superieur à 21
-            if (element.value === 1 && (handScore+element.value) >21){
-                //L'as vaut 1
-                handScore++;
-            } else {
-                handScore += element.score;
-            }
-            cardsToShow.push(`${element.color}${thisValue}`);
+            cardsToShow += `${element.color}${thisValue}|`;          
         });
-        cardsToShow += `(${handScore})`;
+        cardsToShow += `(${handOfCards.actualScore})`;
         console.log(cardsToShow);
-    }
+    },
+    //FONCTION : Calcule le score des deux mains actuelles et mets à jour les valeurs
+    calculateScore : function(){
+        let thisParty = cardGame.thisParty;
+        let players = [thisParty.dealer,thisParty.player];
+        //Player elt renvoie l'objet player
+        players.forEach(function(playerElt){
+            playerElt.actualScore = 0;
+            //playerCards renvoire le sous objet cards de l'objet player
+            playerElt.cards.forEach(function(playerCard){
+                //Si après addition le score < 21 alors la carte vaut 11 
+                if (playerCard.score === 1 && (playerElt.actualScore + playerCard.score) < 21) {
+                    playerCard.score = 11;
+                }
+                playerElt.actualScore += playerCard.score;
+                
+            });
+        });
+    },
+    //FONCTION : MECANIQUE DE JEU DU JOUEUR 
+    playerTurn : function(){
+        let thisParty = cardGame.thisParty;
+        let player = thisParty.player;
+        let playerAction = Number(prompt('Que voulez vous faire?\n 1)Tirer une carte 2)S\'arréter 3)Doubler 4)Spliter'));
+        switch (playerAction){
+            case 1 : 
+                cardGame.pickCard(1,player.cards,thisParty.cardSet);
+                cardGame.calculateScore();  
+                cardGame.displayHandOfCards(player);
+                cardGame.isGameOver();
+                break;
+            default : 'défaut';
+        }
+    },
+    dealerTurn : function(){
+        let thisParty = cardGame.thisParty;
+        let dealer = thisParty.dealer;
+        //Si le dealer a deux cartes && As + 7 ou inversement
+        if(dealer.cards.length < 3 && dealer.cards[0] === 1 && dealer.cards[1] === 7
+            ||dealer.cards.length < 3 && dealer.cards[0] === 7 && dealer.cards[1] === 1){
+            return;
+        } else if (dealer.actualScore < 17){
+            cardGame.pickCard(1,dealer.cards,thisParty.cardSet);
+            cardGame.calculateScore();  
+            cardGame.displayHandOfCards(dealer);
+            cardGame.isGameOver();
+        } else {
+            return;
+        }
 
-
+    },
+    //FONCTION : CONTROLE LE GAME OVER
+    isGameOver : function(){
+        let thisParty = cardGame.thisParty;
+        let playerScore = thisParty.player.actualScore;
+        let dealerScore = thisParty.dealer.actualScore;
+        let winner = null;
+        let looser = null;
+        
+        if (playerScore > 21){
+            winner = thisParty.dealer;
+            looser = thisParty.player;
+            thisParty.isGameOver = true;
+        } else if  (dealerScore > 21){
+            winner = thisParty.player;
+            looser = thisParty.dealer;
+            thisParty.isGameOver = true;
+        }      
+        if (thisParty.isGameOver === true){
+            console.log(`${looser.name} a dépassé les limites (${looser.actualScore}), c'est ${winner.name} qui remporte la partie!`);
+        }
+    },
 };
 
 //EXECUTION DU CODE
