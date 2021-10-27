71baf3564f0c8025c7bcb90fa2ebbbde70e4d41a
Arnaud Peybernes <arnaud.peybernes@gmail.com>
2021-10-27 14:59:17 +0200

    ajoute des fonctions sliceCards et shuffleCards -- OK

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ const cardGame = {
         cardGamesQuantity : 6,
     },
     thisParty : {
-        cards : [],
+        cardSet : [],
     },
     //CREE UN JEUX & MELANGE LES CARTES 
-    start : function(){
+    init : function(){
         //Array qui va stocker toutes les cartes
         //COLLECTION DE JEUX DE CARTE
         let cardGamesCollection = [];
@@ -45,19 +45,56 @@ const cardGame = {
                 }
 
             }
+            //On (mélange + coupe) * 2 
+            thisCardGame = cardGame.shuffleCards(thisCardGame);
+            thisCardGame = cardGame.sliceCards(thisCardGame);
+            thisCardGame = cardGame.shuffleCards(thisCardGame);
+            thisCardGame = cardGame.sliceCards(thisCardGame);
             cardGamesCollection.push(thisCardGame);
-            //Mélange une fois les jeux rassemblés
+            //Si la collection est complète On rassemble les jeux de cartes
             if (numberOfCardGame === cardGame.globalSettings.cardGamesQuantity-1){
-                //On fusionne les 6 tableaux
-                for (var i = 1 ; i < cardGamesCollection.length; i++){
+                for (let i = 1 ; i < cardGamesCollection.length; i++){
                     cardGamesCollection[0] = cardGamesCollection[0].concat(cardGamesCollection[i]);
                 }
                 cardGamesCollection = cardGamesCollection[0];
             }
+           
+
         }
-        cardGame.thisParty.cards.push(cardGamesCollection);
+        //On coupe deux foix l'ensemble des jeux
+        cardGamesCollection = cardGame.sliceCards(cardGamesCollection);
+        cardGamesCollection = cardGame.sliceCards(cardGamesCollection);
+        //On coupe le jeux a une position > 52 cartes
+        cardGamesCollection = cardGame.sliceCards(cardGamesCollection,52);
+        cardGame.thisParty.cardSet.push(cardGamesCollection);
+    },
+    //MELANGE UN JEUX
+    shuffleCards : function(cardGame){
+        let shuffledCardGame =[];
+        while (cardGame.length > 0){         
+            let cardPicked = cardGame.splice(Math.round(Math.random()*cardGame.length-1),1);
+            shuffledCardGame.push(cardPicked[0]);
+           
+        }
+        return shuffledCardGame;
+    },
+    //COUPE UN JEUX 
+    sliceCards : function(cardGame,minCardtoSlice){ 
+        if (minCardtoSlice === undefined){ minCardtoSlice = 0;}
+        let placeWhereISlice = Math.round((Math.random()*(cardGame.length-minCardtoSlice))+minCardtoSlice);
+        let after = cardGame.slice(placeWhereISlice);
+        let before = cardGame.slice (0,placeWhereISlice);
+        if (minCardtoSlice === 0){
+            after.concat(before);
+            return cardGame;
+        } else {
+            console.log(placeWhereISlice);
+            return before;
+        }
+        
     }
 };
 
 //EXECUTION DU CODE
-cardGame.start();
+cardGame.init();
+console.log(cardGame.thisParty.cardSet);
